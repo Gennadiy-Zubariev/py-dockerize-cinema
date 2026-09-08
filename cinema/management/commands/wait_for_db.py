d29d4aaa872bc63a152eabd7532738b2dc8e12d6
Gennadiy-Zubariev <genada89@gmail.com>
2026-09-08 23:25:07 +0300

    dokerize cinema app

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,20 @@
+import time
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    help = "Wait for DB"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Wait for DB...")
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+                db_conn.ensure_connection()
+            except OperationalError:
+                self.stdout.write("DB not available, wait 1 sec.")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("DB available!"))
